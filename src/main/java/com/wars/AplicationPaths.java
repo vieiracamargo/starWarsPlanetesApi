66285ba4ad31d944f83f33173602edf6aecae473
Bruno Camargo <brunovieiracamargo@gmail.com>
2023-11-14 14:02:50 -0300

    feature: change configurations.

--- src/main/java/com/wars/AplicationPaths.java
+++
@@ -1,9 +0,0 @@
-package com.wars;
-
-public class AplicationPaths {
-    public static final String ROOT_PATH = "/planets";
-
-    private AplicationPaths() {
-
-    }
-}
---
+++ src/main/java/com/wars/Paths.java
@@ -0,0 +1,10 @@
+package com.wars;
+
+public class Paths {
+    public static final String ROOT_PATH = "/planets";
+    public static final String STAR_WARS_SERVICE = "/planets";
+
+    private Paths() {
+
+    }
+}
